da57f4b66320148efcdb613a7b4cc03b8cdfc016
MohamedEmad <aleslam12345@gmail.com>
2023-06-29 23:39:08 +0300

    pass color to ColorDetailsVC

--- ColrosTable/Code/ColorsTableVC.swift
+++ ColrosTable/Code/ColorsTableVC.swift
@@ -8,14 +8,35 @@
 import UIKit
 
 class ColorsTableVC: UIViewController  {
+    
+    var colors : [UIColor] = [ ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
- 
+        createRandomColors()
     }
     
+    func createRandomColors(){
+        
+        for _ in 0..<100 {
+            colors.append(createRandomColor())
+        }
+        
+    }
     
+    func createRandomColor () -> UIColor {
+        let randomColor = UIColor (red: CGFloat.random(in: 0...1),
+                                   green: CGFloat.random(in: 0...1),
+                                   blue: CGFloat.random(in: 0...1),
+                                   alpha: 1)
+        
+        return randomColor
+    }
  
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destVc = segue.destination as! ColorDetailVC
+        destVc.color =  sender as? UIColor
+    }
 
 }
 
@@ -23,16 +44,21 @@ extension ColorsTableVC: UITableViewDelegate ,UITableViewDataSource
 {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-       return 50
+        return colors.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-       return UITableViewCell()
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ColorCell")
+        let color = colors[indexPath.row]
+        cell?.backgroundColor = color
+       return cell!
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        performSegue(withIdentifier: " ToColorDetails", sender: nil)
+        let color = colors[indexPath.row]
+        performSegue(withIdentifier: " ToColorDetails", sender: color)
     }
     
 }
- 
+  
